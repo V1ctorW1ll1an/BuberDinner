57446232745bba6965a7763582d84d07517b08f3
Victor Willian <victorwillian1012@gmail.com>
2023-07-21 22:13:54 -0300

    chore: :heavy_check_mark: Add Authorization and Authentication

---
+++ BuberDinner.Api/Controllers/DinnersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+	[Route("[controller]")]
+	public class DinnersController : ApiController
+	{
+		[HttpGet]
+		public IActionResult ListDinners()
+		{
+			return Ok(Array.Empty<string>());
+		}
+	}
+}
+
--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using BuberDinner.Infrastructure.Persistence;
 using BuberDinner.Application.Common.Interfaces.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BuberDinner.Infrastructure;
 
@@ -16,11 +19,35 @@ public static class DependencyInjection
         ConfigurationManager configuration
     )
     {
+        services.AddAuth(configuration);
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings._SectionName, jwtSettings);
         services.Configure<JwtSettings>(configuration.GetSection(JwtSettings._SectionName));
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
-        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
-        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtSettings.Secret)
+                )
+            });
+
         return services;
     }
 }
